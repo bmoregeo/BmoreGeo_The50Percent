293932576c599c5c4fd36f9cc11a5bbf46a61d08
christopherfricke <christopher.fricke@bmoregeo.com>
2014-03-01 11:59:53 -0500

    Add operational layers from config

--- src/js/app/vm/mapVM.js
+++ src/js/app/vm/mapVM.js
@@ -10,15 +10,49 @@ define(['knockout', 'app/config', 'jquery', 'app/model/map'], function(ko, confi
     var mapVM = new function () {
         var self = this;
 
-        var
+        self._basemap = ko.observable();
+        self._center = ko.observable();
+        self._zoom = ko.observable();
+
+        self.basemap = ko.computed({
+            read: function () {
+                return self._basemap;
+            },
+            write: function (value) {
+                self._basemap = value;
+                map.addLayer(value);
+            },
+            owner: self
+        });
+
+        self.center = ko.computed({
+            read: function () {
+                return self._center;
+            },
+            write: function (value) {
+                self._center = value;
+                map.mapInstance.panTo(value);
+            },
+            owner: self
+        });
+
+        self.zoom = ko.computed({
+            read: function () {
+                return self._zoom;
+            },
+            write: function (value) {
+                self._zoom = value;
+                map.mapInstance.setZoom(zoom);
+            },
+            owner: self
+        });
+
+
 
         self.init = function () {
             map.mapInstance.setView(config.map.initialCenter,
                                     config.map.initialZoom);
-            /*
-              Add Basemap Layer
-            */
-            map.addLayer(config.map.baseMap);
+
 
             /*
               Add Operational Layers
