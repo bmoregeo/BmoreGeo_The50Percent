b9843bc67400ec568057dab5bf8c484188a383a1
christopherfricke <christopher.fricke@bmoregeo.com>
2014-03-02 20:29:19 -0500

    Add documentation!

--- src/js/app/vm/mapVM.js
+++ src/js/app/vm/mapVM.js
@@ -13,10 +13,13 @@ define(['knockout', 'app/config', 'jquery', 'app/model/map'], function(ko, confi
         self._basemap = ko.observable();
         self._center = ko.observable();
         self._zoom = ko.observable();
-        self.operationalLayers = ko.observableArray();
 
+        /** * Create a leaflet layer
+         @param {Object} layerInfo - config settings including data, layer settings, and type
+         to generate a leaflet layer
+         @return {Object} - Leaflet Layer Object
+         */
         self.createLayer = function(layerInfo){
-            // Create a layer of open streetmap tile layer
             var layerFunction = function(type){
                 return type === 'tile' ?  L.TileLayer :
                     type === 'wms' ? L.TileLayer.WMS :
@@ -28,6 +31,14 @@ define(['knockout', 'app/config', 'jquery', 'app/model/map'], function(ko, confi
 
         };
 
+        /** * Operational Layers array
+         *
+         * @param {Object} operationalLayer - Operational Layer Configuration
+         */
+        self.operationalLayers = ko.observableArray();
+        /** * Operational layer subscribable adds or deletes a layer in the map object when it gets
+         *      updated in the operationalLayer array
+         */
         self.operationalLayers.subscribe(function(changes){
             for (var i = 0; i < changes.length; i++){
                 if (changes[i].status === 'added'){
@@ -36,23 +47,41 @@ define(['knockout', 'app/config', 'jquery', 'app/model/map'], function(ko, confi
                     map.mapInstance.removeLayer(changes[i].value);
                 }
             }
-            console.log(changes);
         }, null, "arrayChange");
 
-
+        /** * Basemap Layer
+         *
+         * @param {Object} basemap - Basemap Layer Configuration
+         */
         self.basemap = ko.observable();
-        self.basemap.subscribe(function(changes) {
-            map.mapInstance.addLayer(changes[0].value);
+        /** * Basemap subscribable defines the basemap layer in the map object.
+         */
+        self.basemap.subscribe(function(value) {
+            map.mapInstance.addLayer(self.createLayer(value));
         });
 
+        /** * Map Center coordinates
+         *
+         * @param {Object} center - Longitude and latitude object
+         */
         self.center = ko.observable();
-        self.center.subscribe(function(changes){map.mapInstance.panTo(changes[0].value)});
+        /** * center subscribable defines the center of the map object.
+         */
+        self.center.subscribe(function(changes){map.mapInstance.panTo(changes.value)});
 
+        /** * Map Zoom Level
+         *
+         * @param {int} zoom - Zoom level of map view
+         */
         self.zoom = ko.observable();
-        self.zoom.subscribe(function(changes){map.mapInstance.setZoom(changes[0].value)});
+        /** * zoom subscribable defines the Zoom level of the map object.
+         */
+        self.zoom.subscribe(function(changes){map.mapInstance.setZoom(changes.value)});
 
 
-
+        /** * Initializes the map object and view
+         *
+         */
         self.init = function () {
             map.mapInstance.setView(config.map.initialCenter,
                                     config.map.initialZoom);
